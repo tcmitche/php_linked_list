a118a0f7c79708a0ff74e27db1435800c6264de7
Travis Mitchell <mortsubite333@gmail.com>
2013-04-11 15:14:30 -0700

    Added the Node class

---
+++ Node.php
@@ -0,0 +1,38 @@
+<?php
+
+class Node
+{
+    private $prev;
+    private $next;
+    private $data;
+    
+    public function __construct($data = null)
+    {
+        $this->data = $data;
+    }
+    
+    public function getData()
+    {
+        return $this->data;
+    }
+    
+    public function setPrev(Node &$node)
+    {
+        $this->prev = $node;
+    }
+    
+    public function getPrev()
+    {
+        return $this->prev;
+    }
+    
+    public function setNext(Node &$node)
+    {
+        $this->next = $node;
+    }
+    
+    public function getNext()
+    {
+        return $this->next;
+    }
+}
